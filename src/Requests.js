6eaa6fbf3077ecd7f7877d9c9618e7ab70dd1342
kewablay <kojokewajunior123@gmail.com>
2022-07-11 16:37:09 +0000

    added changes --frontend complete

--- src/Requests.js
+++ src/Requests.js
@@ -3,7 +3,7 @@ const API_KEY = "f958e57872b762859dc59c4068feca94";
 const requests = {
   fetctTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRated: `/discover/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchTopRated: `movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
--- src/components/Row.js
+++ src/components/Row.js
@@ -23,16 +23,20 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
     <div className="row">
       <h2 className="row__title">{title}</h2>
       <div className="row__posters">
-        {movies.map((movie) => (
-          <img
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            src={`${base_url}${
-              isLargeRow ? movie?.poster_path : movie?.backdrop_path
-            }`}
-            alt={movie?.name}
-            key={movie?.id}
-          />
-        ))}
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`${base_url}${
+                  isLargeRow ? movie?.poster_path : movie?.backdrop_path
+                }`}
+                alt={movie?.name}
+                key={movie?.id}
+              />
+            )
+        )}
       </div>
     </div>
   );
